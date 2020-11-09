751b5564661e97c96055b66e705b42783bde6082
Holson Mpangala <holsonm74@gmail.com>
2020-11-09 08:52:02 +0100

    Update Members Files

--- news/application/views/members.php
+++ news/application/views/members.php
@@ -5,8 +5,8 @@
             <div class="row">
                 <div class="col-md-9">
                     <div class="title-base text-left">
-                        <h1>Frequently asked questions</h1>
-                        <p>Listed questions and answers, all supposed to be commonly asked in some context, and pertaining to a particular topic.</p>
+                        <h1><?php echo html_escape($title); ?></h1>
+                        <p>L<?php echo html_escape($description); ?></p>
                     </div>
                 </div>
                 <div class="col-md-3">
@@ -20,3 +20,160 @@
             </div>
         </div>
     </div>
+
+
+
+    <div class="section-empty section-item text-center">
+        <div class="container content">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="advs-box niche-box-team" data-anima="scale-up" data-trigger="hover">
+                        <a class="img-box">
+                            <img class="anima" src="../images/users/user-6.jpg" alt="" />
+                        </a>
+                        <div class="content-box">
+                            <h2>Jack Christian</h2>
+                            <h4>Founder and ceo</h4>
+                            <hr class="e" />
+                            <div class="btn-group social-group">
+                                <a target="_blank" href="#"><i class="fa fa-facebook"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-twitter"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                            <p>Lorem ipsum is text of the printing and industry manulo pertus.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="advs-box niche-box-team" data-anima="scale-up" data-trigger="hover">
+                        <a class="img-box">
+                            <img class="anima" src="../images/users/user-7.jpg" alt="" />
+                        </a>
+                        <div class="content-box">
+                            <h2>Zarata Afratto</h2>
+                            <h4>Language expert</h4>
+                            <hr class="e" />
+                            <div class="btn-group social-group">
+                                <a target="_blank" href="#"><i class="fa fa-facebook"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-twitter"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                            <p>Lorem ipsum is text of the printing and industry manulo pertus.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="advs-box niche-box-team" data-anima="scale-up" data-trigger="hover">
+                        <a class="img-box">
+                            <img class="anima" src="../images/users/user-2.jpg" alt="" />
+                        </a>
+                        <div class="content-box">
+                            <h2>Sarah Partison</h2>
+                            <h4>Co-founder</h4>
+                            <hr class="e" />
+                            <div class="btn-group social-group">
+                                <a target="_blank" href="#"><i class="fa fa-facebook"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-twitter"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                            <p>Lorem ipsum is text of the printing and industry manulo pertus.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="advs-box niche-box-team" data-anima="scale-up" data-trigger="hover">
+                        <a class="img-box">
+                            <img class="anima" src="../images/users/user-3.jpg" alt="" />
+                        </a>
+                        <div class="content-box">
+                            <h2>Lee Chen Tol</h2>
+                            <h4>Marketing expert</h4>
+                            <hr class="e" />
+                            <div class="btn-group social-group">
+                                <a target="_blank" href="#"><i class="fa fa-facebook"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-twitter"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                            <p>Lorem ipsum is text of the printing and industry manulo pertus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr class="space" />
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="advs-box niche-box-team" data-anima="scale-up" data-trigger="hover">
+                        <a class="img-box">
+                            <img class="anima" src="../images/users/user-8.jpg" alt="" />
+                        </a>
+                        <div class="content-box">
+                            <h2>Alan Martinez</h2>
+                            <h4>Marketing</h4>
+                            <hr class="e" />
+                            <div class="btn-group social-group">
+                                <a target="_blank" href="#"><i class="fa fa-facebook"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-twitter"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                            <p>Lorem ipsum is text of the printing and industry manulo pertus.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="advs-box niche-box-team" data-anima="scale-up" data-trigger="hover">
+                        <a class="img-box">
+                            <img class="anima" src="../images/users/user-9.jpg" alt="" />
+                        </a>
+                        <div class="content-box">
+                            <h2>Zarata Afratto</h2>
+                            <h4>Media relations</h4>
+                            <hr class="e" />
+                            <div class="btn-group social-group">
+                                <a target="_blank" href="#"><i class="fa fa-facebook"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-twitter"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                            <p>Lorem ipsum is text of the printing and industry manulo pertus.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="advs-box niche-box-team" data-anima="scale-up" data-trigger="hover">
+                        <a class="img-box">
+                            <img class="anima" src="../images/users/user-4.jpg" alt="" />
+                        </a>
+                        <div class="content-box">
+                            <h2>Sarah Partison</h2>
+                            <h4>Co-founder</h4>
+                            <hr class="e" />
+                            <div class="btn-group social-group">
+                                <a target="_blank" href="#"><i class="fa fa-facebook"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-twitter"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                            <p>Lorem ipsum is text of the printing and industry manulo pertus.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="advs-box niche-box-team" data-anima="scale-up" data-trigger="hover">
+                        <a class="img-box">
+                            <img class="anima" src="../images/users/user-5.jpg" alt="" />
+                        </a>
+                        <div class="content-box">
+                            <h2>Lee Chen Tol</h2>
+                            <h4>Marketing expert</h4>
+                            <hr class="e" />
+                            <div class="btn-group social-group">
+                                <a target="_blank" href="#"><i class="fa fa-facebook"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-twitter"></i></a>
+                                <a target="_blank" href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                            <p>Lorem ipsum is text of the printing and industry manulo pertus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   
