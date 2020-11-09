43fc4618d2cf98c2f594f4d5da9339ee0da017e9
Holson Mpangala <holsonm74@gmail.com>
2020-11-09 03:25:19 +0100

    Create faq page

--- news/application/controllers/Home_controller.php
+++ news/application/controllers/Home_controller.php
@@ -134,6 +134,27 @@ class Home_controller extends Home_Core_Controller
 		}
 	}
 
+	public function faq()
+	{
+		get_method();
+		$data['page'] = $this->page_model->get_page('faq');
+		//check page auth
+		$this->checkPageAuth($data['page']);
+
+		if ($data['page']->page_active == 0) {
+			$this->error_404();
+		} else {
+			$data['title'] = get_page_title($data['page']);
+			$data['description'] = get_page_description($data['page']);
+			$data['keywords'] = get_page_keywords($data['page']);
+			
+
+			$this->load->view('partials/_header', $data);
+			$this->load->view('contact', $data);
+			$this->load->view('partials/_footer');
+		}
+	}
+
 
 	/**
 	 * Contact Page Post
