28f3944688aeb0c09a936b886909a815b5441b31
Holson Mpangala <holsonm74@gmail.com>
2020-11-09 07:33:45 +0100

    Update Members.php views

--- news/application/views/members.php
+++ news/application/views/members.php
@@ -1,1 +1,22 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+    
+    <div class="header-base white">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="title-base text-left">
+                        <h1>Frequently asked questions</h1>
+                        <p>Listed questions and answers, all supposed to be commonly asked in some context, and pertaining to a particular topic.</p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <ol class="breadcrumb b white">
+                    <li class="breadcrumb-item">
+                            <a href="<?php echo lang_base_url(); ?>"><?php echo html_escape(trans("home")); ?></a>
+                        </li>
+                        <li class="breadcrumb-item active">Faqs</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
